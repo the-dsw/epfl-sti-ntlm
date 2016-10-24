8a27d0e1a0a59f26f6f53c057fa15f3f77c80069
the-dsw <fmod@the-dsw.com>
2016-10-24 12:29:40 +0200

    server changed

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 // set up ======================================================================
-var http			       = require('http');
-var httpntlm         = require('httpntlm');               // authentication NTLM
-var fs               = require('fs');
-var archiver         = require('archiver');              // compression ZIP
-var csv              = require('csv');                   // csv files
-var session	    	   = require('express-session');
-var express			     = require('express');
-var app				       = exports.app = express();					  // create our app with express
-var port			       = process.env.PORT || 8000;					// set the port
-var morgan			     = require('morgan');
-var bodyParser		   = require('body-parser');
-var methodOverride	 = require('method-override');
+var http = require('http');
+var httpntlm = require('httpntlm'); // HTTP authentication NTLM
+var fs = require('fs');
+var archiver = require('archiver'); // compression ZIP
+var csv = require('csv'); // CSV files
+var session = require('express-session');
+var express = require('express');
+var app = exports.app = express(); // create our app with express
+var port = process.env.PORT || 8000; // set the port
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // Mongoose ====================================================================
 require('./config/database');
 
 // Express =====================================================================
 
-app.use(express.static(__dirname + '/public'));					// set the static files location /public/img will be /img for users
-app.use(morgan('dev'));											// log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));			// parse application/x-www-form-urlencoded
-app.use(bodyParser.json());										// parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override'));				// override with the X-HTTP-Method-Override header in the request
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 //app.use(session({ secret: 'sampleSecretSession', resave: true, saveUninitialized: true }));
 
-// Cross Domain
+// Cross Domain ================================================================
 app.use(function(request, response, next) {
     response.header('Access-Control-Allow-Credentials', true);
     response.header('Access-Control-Allow-Origin', request.headers.origin);
@@ -35,8 +39,8 @@ app.use(function(request, response, next) {
     next();
 });
 
-// Serveur ====================================================================
-var server           = http.Server(app);
+// Server ======================================================================
+var server = http.Server(app);
 
 // Routes ======================================================================
 //require('./app/routes')(app);
@@ -44,8 +48,8 @@ var server           = http.Server(app);
 //require('./app/email/email')(app);
 
 process.on('SIGINT', function() {
-  console.log("\nStopping...");
-  process.exit();
+    console.log("\nStopping...");
+    process.exit();
 });
 
 // listen (start app with node server.js) ======================================
