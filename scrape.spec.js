9193d300af70588c372d5202059d7d10f71a90b3
the-dsw <fmod@the-dsw.com>
2016-11-01 10:05:39 +0100

    included test unit

---
+++ scrape.spec.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var Scrape = require('./scrape.js');
+
+describe('Scrape Zip data from Modulotech', function(){
+
+    it('Should be loaded', function(){
+        assert.equal(true, true);
+    })
+
+})
